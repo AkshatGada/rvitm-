1be6ffb7c053cadc71fe27c52a801afb5f2118aa
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2024-01-24 01:36:45 +0530

    lesgo

---
+++ Nftexp.sol
@@ -0,0 +1,45 @@
+// What is pinata ?
+// Pinata is a service for the InterPlanetary File System (IPFS),
+//  a decentralized and distributed file storage system. Pinata provides tools and services
+//  to simplify the usage of IPFS, making it more accessible for developers and users.
+
+
+// What is a cid 
+// The string "QmeQQ7We5r3ShgK6tK5Jdh5UAeGi2Lj1TZs9DLfTz5TqNJ" is an IPFS (InterPlanetary File System) content identifier.
+//  When you upload a file to IPFS, it gets assigned a unique content identifier, known as a CID (Content Identifier). 
+//  This CID is essentially a hash of the file's content, and it serves as a unique identifier for that specific file.
+ // https://ipfs.io/ipfs/QmeQQ7We5r3ShgK6tK5Jdh5UAeGi2Lj1TZs9DLfTz5TqNJ
+
+ //What is metadata of a file ?
+//  Metadata refers to additional information about a file that provides context and describes various attributes associated with the file.
+//   It does not include the actual content of the file but offers details about the file itself
+ // File Name: document.pdf
+// File Type/Format: PDF (Portable Document Format)
+// Size: 2.5 MB
+// Creation Date: January 15, 2022, 09:30 AM
+// Last Modified Date: February 20, 2022, 03:45 PM
+// Author/Creator: John Doe
+// Permissions: Read/Write for Owner, Read-Only for Group, No Access for Others
+// Location/Path: /Documents/Reports/
+// Checksum/Hash: SHA-256: a3e7b25e9d4a22c16c3a6c31731f8e15074bfbea64f44e7e4c8ec3a14a60cf6d
+// Version Number: v2.0
+// Description/Comments: Quarterly Financial Report for Q4 2021
+// Keywords/Tags: Finance, Reports, Q4, 2021
+// Associated Software: Adobe Acrobat Reader
+// Exif Data (for Images):
+// Camera Model: Canon EOS 5D Mark IV
+// Exposure Time: 1/250 seconds
+// Aperture: f/2.8
+// ISO: 400
+// Audio/Video Metadata (for an MP3 file):
+// Artist: Adele
+// Album: 21
+// Genre: Pop
+// Duration: 4 minutes, 18 seconds
+// Codec: MP3
+
+
+// What is ipfs ?
+// IPFS, or the InterPlanetary File System, is a protocol and network designed to create a content-addressable, 
+// peer-to-peer method of storing and sharing hypermedia in a distributed file system. It aims to make the web more decentralized, efficient,
+//  and resilient by changing the way files are addressed and served on the internet.
