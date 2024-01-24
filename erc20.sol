740340b0f7f5ec1d07fd1da5bea397e5d4b50077
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2024-01-24 12:43:34 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,10 +1,13 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000000000 * 10**decimals());
-    }
+contract ShardeumERC20Token is ERC20 {
+
+   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
+       _mint(msg.sender, 10000 * 10 ** 18);
+
+   }
 }
